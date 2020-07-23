ee202f377aa18bd04bbd0593c793920bf4b02e96
AliceRaffaele <alice.raffaele@unitn.it>
2020-07-23 17:16:19 +0200

    aggiornate istruzioni riconsegna

--- generate_all_exams_given_list.py
+++ generate_all_exams_given_list.py
@@ -40,8 +40,6 @@ if __name__ == "__main__":
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
-            if line_count == 3:
-                break
             badge_nb = row[1]
             s = row[3]
             student_ID = row[5].split('@')[0]
