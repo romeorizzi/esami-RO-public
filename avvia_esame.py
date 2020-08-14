adeb61d7ba76a59209e9ac6797f904394bd148c5
Adriano Tumminelli <adriano.tummi@hotmail.it>
2020-08-14 18:29:49 +0200

    log file + moved students_lists directory

--- avvia_esame.py
+++ avvia_esame.py
@@ -1,11 +1,46 @@
+
 import sys
+import os
+
+
+#########################
+#  Settings
+#########################
+conda_environment = sys.argv[1] if len(sys.argv) == 2 else "ROexam"
+webserverPort = 8080
+webserverIp = "127.0.0.1"
+start_port = 8888
+pythonPath = sys.executable
+
+#########################
+#  Create the logger
+#########################
+class Logger(object):
+    def __init__(self, fname, terminal):
+        self.terminal = terminal
+        self.log = open(fname, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)  
+
+    def flush(self):
+        #this flush method is needed for python 3 compatibility.
+        #this handles the flush command by doing nothing.
+        #you might want to specify some extra behavior here.
+        pass    
+
+if not os.path.exists("log"):
+    os.mkdir("log")
+
+sys.stdout = Logger("log/out.log", sys.stdout)
+sys.stderr = Logger("log/errors.log", sys.stderr)
 
 #Blocca python versioni di python inferiori a 2
 if sys.version_info[0] <= 2:
     print("Questo script richiede python 3 installato in un Conda Environment")
     exit()
 
-import os
 import subprocess
 import webbrowser
 import signal
@@ -14,17 +49,11 @@ import importlib
 if sys.version_info[1] > 4:
     from importlib import util
  
-conda_environment = sys.argv[1] if len(sys.argv) == 2 else "ROexam"
-webserverPort = 8080
-webserverIp = "127.0.0.1"
-start_port = 8888
-pythonPath = sys.executable
-
 print(
     "=================================\n" +
-    "Python versione " + str(sys.version_info[0]) + "." + str(sys.version_info[1]) + "\n" + 
+    "Python version " + str(sys.version_info[0]) + "." + str(sys.version_info[1]) + "\n" + 
+    "Python path: " + str(sys.exec_prefix) + "\n" + 
     "Sistema Operativo: " + str(sys.platform) + "\n" + 
-    "Percorso: " + str(sys.exec_prefix) + "\n" + 
     "=================================\n"
 )
 
@@ -32,7 +61,6 @@ print(
 if conda_environment not in sys.exec_prefix:
     print("ATTENZIONE:\nEsegui questo script dal giusto conda environment: " + conda_environment)
     exit()
-    
 
 def module_exists(name):
     if sys.version_info[0] == 3:
@@ -60,6 +88,7 @@ def tryPort(port):
     sock.close()
     return ret
 
+# Ottieni una porta libera per jupyter
 target_port = start_port
 while not tryPort(target_port):
     target_port += 1
@@ -67,12 +96,12 @@ while not tryPort(target_port):
 jupyterPort = str(target_port)
 print("La porta che verra' utilizzata per Jupyter e': " + jupyterPort + "\n")
 
-
 conda_env_path = sys.exec_prefix
 
 # start jupyter notebook
 proc_jupyter = subprocess.Popen(["python", '-m', 'notebook', "--NotebookApp.token=''", "--no-browser", "--port", jupyterPort])
 
+# Ottieni una porta libera per il server http della mappa
 target_port = webserverPort
 while not tryPort(target_port):
     target_port += 1
@@ -80,20 +109,14 @@ while not tryPort(target_port):
 webserverPort = str(target_port)
 print("La porta che verra' utilizzata per il Server e': " + webserverPort + "\n")
 
-# start local http server
-# command = sys.executable + " map/server.py " + str(webserverPort) + " --bind " + webserverIp 
-# print("> " + command)
-# proc_server = subprocess.Popen(command, shell=True)
-
 proc_server = subprocess.Popen(["python", "map/server.py", webserverPort, "--bind", webserverIp])
 webbrowser.open("http://" + webserverIp + ":" + webserverPort + "/map/index.html?port="+jupyterPort)
 
 try:
     proc_jupyter.communicate()
     proc_server.communicate()
-    print("COMMUNICATED")
 except KeyboardInterrupt:
     proc_jupyter.terminate()
     proc_server.terminate()
-    print("\n----Closing program---\n")
+    print("\n----Chiusura del processo jupyter---\n")
     proc_close_jupyter = subprocess.Popen(["python", "-m", "notebook", "stop", jupyterPort])
